c1280f4c12ec97ee31e239e45e75ed3989765a20
felipecapistrano <felipecmaes@gmail.com>
2020-07-08 12:55:31 -0300

    Commit inicial

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FullstackRPG.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FullstackRPG.WebApiApplication" Language="C#" %>
