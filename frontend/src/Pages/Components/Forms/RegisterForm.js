2c05428d4ce1520aafefa4cd2170d40e49429f77
felipecapistrano <felipecmaes@gmail.com>
2020-08-05 15:46:25 -0300

    Primeira amostra

---
+++ frontend/src/Pages/Components/Forms/RegisterForm.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { useFormik } from "formik"
+
+function RegisterForm () {
+    const formik = useFormik(
+        
+    )
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default RegisterForm
